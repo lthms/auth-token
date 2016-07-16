707e1de04c8e47efccb0fff42705e6bc1efd42df
Thomas Letan <contact@thomasletan.fr>
2016-07-16 19:10:09 +0200

    Fix Aeson from/toJSON implementation for tokens

--- token-aeson/src/Data/Token/Aeson.hs
+++ token-aeson/src/Data/Token/Aeson.hs
@@ -4,9 +4,15 @@ import           Data.Aeson
 import           Data.Aeson.Types
 import           Data.Token
 import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import           Data.ByteString.Base64 as B64
+import           Control.Monad
 
 instance ToJSON (Token scope) where
-    toJSON = String . decodeUtf8 . toByteString
+    toJSON = String . decodeUtf8 . B64.encode . toByteString
 
 instance FromJSON (Token scope) where
-    parseJSON (String val) = return . fromByteString . encodeUtf8 $ val
+    parseJSON (String val) =
+      case B64.decode $ encodeUtf8 val of
+        Left _   -> mzero
+        Right bs -> return $ fromByteString bs
+    parseJSON _ = mzero
